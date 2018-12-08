f0744392bab9eeb33d7a6ec7af23c188099b3350
aniagarwal97 <aniagarwal97@gmail.com>
2018-12-08 19:52:46 +0530

    Added links in Profile page

--- src/Container/Profile/index.js
+++ src/Container/Profile/index.js
@@ -23,25 +23,28 @@ export default class Profile extends Component {
                     <p>
                         My personal website is on address</p>
                     <ul>
-                        <li>meelisfotography.com</li>
-                        <li>http://www.redbubble.com/people/bykowski/journal/14297535-adams-photographer-of-the-day-is-loiteke#comment-58636506</li>
-                        <li>http://solo-exhibition.com/workshops/badges/Solo-accepted-work-Masterpiece-Workshop-Catalog.png</li>
+                        <li><a href="meelisfotography.com">meelisfotography.com</a></li>
+                        <li><a href="http://www.redbubble.com/people/bykowski/journal/14297535-adams-photographer-of-the-day-is-loiteke#comment-58636506">http://www.redbubble.com/people/bykowski/journal/14297535-adams-photographer-of-the-day-is-loiteke#comment-58636506</a></li>
+                        <li><a href="http://solo-exhibition.com/workshops/badges/Solo-accepted-work-Masterpiece-Workshop-Catalog.png">http://solo-exhibition.com/workshops/badges/Solo-accepted-work-Masterpiece-Workshop-Catalog.png</a></li>
                     </ul>
                         <p>My images may not be used wholly or in part without prior written permission, including copying, duplicating, printing, publishing (even on a website), pinning (Pinterest), reproducing, storing, sharing or transmitting by any means whatsoever.</p>
                         
                         <p>View my photo website at:</p>
                         <ul>
                             <li>
-                                http://www.redbubble.com/people/bykowski/journal/14297535-adams-photographer-of-the-day-is-loiteke#comment-58636506
+                                <a href="http://www.redbubble.com/people/bykowski/journal/14297535-adams-photographer-of-the-day-is-loiteke#comment-58636506">http://www.redbubble.com/people/bykowski/journal/14297535-adams-photographer-of-the-day-is-loiteke#comment-58636506</a>
+                            </li>
+                            <li><a href="http://society6.com/MeeliLoiteSonn">
+                                http://society6.com/MeeliLoiteSonn</a>
+                            </li>
+                            <li><a href = "http://society6.com/meeliloitesonn?promo=B96KMB2GVCGY">
+                            http://society6.com/meeliloitesonn?promo=B96KMB2GVCGY
+                            </a>
                             </li>
                             <li>
-                                http://society6.com/MeeliLoiteSonn
-                            </li>
-                            <li>
-                                http://society6.com/meeliloitesonn?promo=B96KMB2GVCGY
-                            </li>
-                            <li>
+                                <a href="http://www.redbubble.com/people/loiteke/works/1771971-sauna-ruins?p=framed-print">
                                 http://www.redbubble.com/people/loiteke/works/1771971-sauna-ruins?p=framed-print
+                                </a>
                             </li>
                         </ul>
                         <p>
@@ -50,27 +53,39 @@ export default class Profile extends Component {
                         </p>
                         <ul>
                             <li>
+                                <a href="http://www.redbubble.com/people/bykowski/journal/14297535-adams-photographer-of-the-day-is-loiteke#comment-58636506">
                                 http://www.redbubble.com/people/bykowski/journal/14297535-adams-photographer-of-the-day-is-loiteke#comment-58636506
+                                </a>
                             </li>
                             <li>
+                                <a href = "http://www.comments.zingerbugimages.com/glitter_graphics/best_of_the_best.gif">
                                 http://www.comments.zingerbugimages.com/glitter_graphics/best_of_the_best.gif
+                                </a>
                             </li>
                             <li>
+                                <a href="http://i.picasion.com/pic70/9fa7b475d9c62756691c4e08cae49efd.gif">
                                 http://i.picasion.com/pic70/9fa7b475d9c62756691c4e08cae49efd.gif
+                                </a>
                             </li>
                             <li>
+                                <a href="http://i.picasion.com/pic70/a23f6334c2b4b27403a577ce1c943a56.gif">
                                 http://i.picasion.com/pic70/a23f6334c2b4b27403a577ce1c943a56.gif
+                                </a>
                             </li>
                             <li>
+                                <a href="http://www.redbubble.com/explore/featured+photography?page=3">
                                 http://www.redbubble.com/explore/featured+photography?page=3
+                                </a>
                             </li>
                             <li>
+                                <a href="http://www.viewbug.com/member/meeliloitesonn">
                                 http://www.viewbug.com/member/meeliloitesonn
+                                </a>
                             </li>
                         </ul>
                         <p>
                         To make a PURCHASE, select an image then CLICK the buy/preview button and choose your product. Alternatively, email me at <a href="mailto:loitekeso@gmail.com">loitekeso@gmail.com</a>, for custom prints.</p>
-                        <p>"I made a sale, thanks for unknown buyer": http://www.redbubble.com/people/loiteke/works/4211180-mustic-red-and-black </p>
+                        "I made a sale, thanks for unknown buyer": <a href="http://www.redbubble.com/people/loiteke/works/4211180-mustic-red-and-black">http://www.redbubble.com/people/loiteke/works/4211180-mustic-red-and-black</a>
                         
                         <p>"To find the universal elements enough;
                         to find the air and the water exhilarating;
@@ -83,22 +98,24 @@ export default class Profile extends Component {
                         <p>These are some of the rewards of the simple life.</p>
                     <ul>
                         <li>
-                            <strong>Web in fineartamerica</strong>: http://fineartamerica.com/profiles/meeli-sonn.html
+                            <strong>Web in fineartamerica</strong>: <a href="http://fineartamerica.com/profiles/meeli-sonn.html">http://fineartamerica.com/profiles/meeli-sonn.html</a>
                         </li>
                         <li>
-                            <strong>Go Maailma blogid</strong>: http://blog.gomaailm.ee/?preview=true&p=6636&time=1358473960
+                            <strong>Go Maailma blogid</strong>: <a href="http://blog.gomaailm.ee/?preview=true&p=6636&time=1358473960">http://blog.gomaailm.ee/?preview=true&p=6636&time=1358473960</a>
                         </li>
                         <li>
+                            <a href="https://reisikirjad.gotravel.ee/author/msonnikasgmail-com/">
                             https://reisikirjad.gotravel.ee/author/msonnikasgmail-com/
+                            </a>
                         </li>    
                         <li>
-                        <strong>Maa müsteerimud</strong>: http://blog.gomaailm\ee/?preview=true&p=5910&time=1358474170
+                        <strong>Maa müsteerimud</strong>: <a href="http://blog.gomaailm\ee/?preview=true&p=5910&time=1358474170">http://blog.gomaailm\ee/?preview=true&p=5910&time=1358474170</a>
                         </li>
                         <li>
-                            <strong>Here is one of my books. It is in Estonian</strong>: http://hallkalliskivi.blogspot.com/2011/07/hall-kalliskivi.html
+                            <strong>Here is one of my books. It is in Estonian</strong>: <a href="http://hallkalliskivi.blogspot.com/2011/07/hall-kalliskivi.html">http://hallkalliskivi.blogspot.com/2011/07/hall-kalliskivi.html</a>
                         </li>
                         <li>
-                            <strong>My Gallery in Host Hullabaloo</strong>: http://www.redbubble.com/groups/solo-exhibition/forums/11563/topics/195865-meeli-sonn-gallery
+                            <strong>My Gallery in Host Hullabaloo</strong>: <a href="http://www.redbubble.com/groups/solo-exhibition/forums/11563/topics/195865-meeli-sonn-gallery">http://www.redbubble.com/groups/solo-exhibition/forums/11563/topics/195865-meeli-sonn-gallery</a>
                         </li>
                     </ul>   
                     <p>                     
@@ -106,28 +123,36 @@ export default class Profile extends Component {
                     </p>
                     <ul>
                         <li>
+                            <a href="http://solo-exhibition.com/images2/Host_Hullabaloo2011_150x150.jpg">
                             http://solo-exhibition.com/images2/Host_Hullabaloo2011_150x150.jpg
+                            </a>
                         </li>
                         <li>
+                            <a href="http://ih0.redbubble.net/image.12518600.2319/flat,800x800,075,f.u1.jpg">
                             http://ih0.redbubble.net/image.12518600.2319/flat,800x800,075,f.u1.jpg
+                            </a>
                         </li>
                         <li>
+                            <a href="http://ih3.redbubble.net/image.10792258.2198/flat,300x300,075,t.jpg">
                             http://ih3.redbubble.net/image.10792258.2198/flat,300x300,075,t.jpg
+                            </a>
                         </li>
                         <li>
+                            <a href="http://www.redbubble.com/groups/new-creations/forums/13440/topics/328067-angel-artist-of-the-week-meeli-sonn-lovely-photography-i-hope-you-visit-her-and-congratulate">
                             http://www.redbubble.com/groups/new-creations/forums/13440/topics/328067-angel-artist-of-the-week-meeli-sonn-lovely-photography-i-hope-you-visit-her-and-congratulate
+                            </a>
                         </li>
                     </ul>
                         <p>
                         I own copyrights on all my photographs on this website. My images may not be posted in another web page on the internet or intranet, may not be published or used for commercial use of any kind without my written permission.
                         </p>
 
-                        <li>http://ih2.redbubble.net/image.11247302.0009/flat,300x300,075,t.jpg</li>
+                        <li><a href="http://ih2.redbubble.net/image.11247302.0009/flat,300x300,075,t.jpg">http://ih2.redbubble.net/image.11247302.0009/flat,300x300,075,t.jpg</a></li>
                         <p>
                         My cameras are Nikon D5200, and has two lenses Nixon DX 18-55mm and a Sigma APO DG 70-300.
                         I am host/co-host of 10 groups which I love all.</p> 
-                        <li>http://ih2.redbubble.net/image.12086228.6784/pp,375x360.jpg</li>
-                        <li>http://ih3.redbubble.net/image.12035062.6731/flat,550x550,075,f.jpg</li>
+                        <li><a href="http://ih2.redbubble.net/image.12086228.6784/pp,375x360.jpg">http://ih2.redbubble.net/image.12086228.6784/pp,375x360.jpg</a></li>
+                        <li><a href="http://ih3.redbubble.net/image.12035062.6731/flat,550x550,075,f.jpg">http://ih3.redbubble.net/image.12035062.6731/flat,550x550,075,f.jpg</a></li>
                         
                         <p>I love these quotes:</p>
 
@@ -137,8 +162,8 @@ export default class Profile extends Component {
                         <li>Sometimes I do get to places just when God's ready to have somebody click the shutter. ~Ansel Adams</li>
                         <p>
                         My hobby is also poetry. I wasn’t interested in poetry until I was older. Then I happened to read a poem by the Estonian poet Alliksaar. I wept, and started my long journey in the promised land of poetry. I consider the world of poems is a golden world, a model, and metaphor. My poems makes references to our world through analogy, and like all analogies it invites being explored "as if" it were real. I think the power of poetry and photography is to refine and mature our awareness, to give us a language in which to frame experience we might otherwise be unable to articulate and therefor to reflect upon. And eyes for both of them are important.
-                        !http://ih0.redbubble.net/work.5734685.2.flat,550x550,075,f.challenge-gold.jpg!
-                        You are welcome to write me. loitekeso@gmail.com
+                        <a href="http://ih0.redbubble.net/work.5734685.2.flat,550x550,075,f.challenge-gold.jpg">http://ih0.redbubble.net/work.5734685.2.flat,550x550,075,f.challenge-gold.jpg</a>
+                        You are welcome to write me. <a href="mailto:loitekeso@gmail.com">loitekeso@gmail.com</a>
                     </p>
                 </div>
                 </div>
