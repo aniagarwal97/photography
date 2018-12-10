d0225319edeeb77c4793da98f3ad2e29fe279c60
aniagarwal97 <aniagarwal97@gmail.com>
2018-12-10 09:31:06 +0530

    Added linkedin and google plus links in footer

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -12,6 +12,8 @@ export default class Footer extends Component {
 						<li><a href="https://www.facebook.com/"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
 						<li><a href="https://twitter.com/"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
 						<li><a href="https://www.instagram.com/"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
+						<li><a href="https://www.linkedin.com/in/meeli-sonn-30246b50/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+						<li><a href="https://meeli-sonn.pixels.com/index.html?utm_source=GooglePlus"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
 					</ul>
 				</div>
 			</footer>
